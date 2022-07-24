033df6258ee4c18acb4befdde68105e77c168541
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-07-24 19:10:14 +0300

    Completed AuthorService

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,9 @@ class Library
   attr_accessor :authors, :books, :readers, :orders
 
   def initialize
-
+    @authors = AuthorService::get_all_authors
   end
-
+  #search in array top reader
   def find_top_reader
 
   end
@@ -21,18 +21,94 @@ class Library
 
   end
 
+  def update
+    @authors = AuthorService::get_all_authors
+  end
+
   def self.print_instruction
 
     puts "(Write command by typing <entity, command>) \n Chose action:"
-    puts "\t\nAuthor\n1) Add \n2) Show \n3) Show Books \n4) Delete"
+    puts "  Author         Book                       Reader          Order"
+    puts "1) Add         1) Add                     1) Add          1) Add"
+    puts "2) Show        2) Show                    2) Show         2) Show"
+    puts "3) Show Books  3) Show most popular book  3) Show Books"
+    puts "4) Delete      4) Delete                  4) Delete       4) Delete"
 
-    puts "\t\nBook\n1) Add \n2) Show \n3) Show most popular books \n4) Delete"
+  end
+end
 
-    puts "\t\nReader\n1) Add \n2) Show \n3) Show Reader's books \n4) Delete"
+library = Library.new
+puts "\n\n\tWelcome to Library App\n"
 
-    puts "\t\nOrder\n1) Add \n2) Show \n3) Delete Order"
+isExit = false
 
-    puts "\nWrite 'Exit' to finish"
+while isExit != true
 
+  Library.print_instruction
+
+  print "\nEnter command: "
+  action = gets.chop
+
+  commands = action.split(", ")
+
+  case commands[0]
+  when "Book"
+    puts "Book"
+
+  when "Reader"
+    puts "Reader"
+
+  when "Order"
+    puts "Order"
+
+  when "Author"
+    if commands[1] == "Show"
+      puts "\n\t Authors"
+      authors = library.authors
+      authors.each do |author|
+        author.to_s
+      end
+      gets
+    elsif commands[1] == "Add"
+      # Check if Author is already exits
+      puts "\n\tAdd Author:"
+      print "Name: "
+      name = gets.chomp
+      print "Biography: "
+      bio = gets.chomp
+
+      if AuthorService::add_author? name, bio
+        puts "\n!- Author was added\n"
+      else
+        puts "\n!- Error!, Something went wrong\n"
+      end
+      library.update
+      gets
+
+    elsif commands[1] == "Delete"
+      # Check if Author is not exits
+      puts "\n\tDelete Author"
+      print "Enter name: "
+      name = gets.chomp
+      print "Enter biography: "
+      bio = gets.chomp
+      if AuthorService.delete_author?(Author.new name, bio)
+        library.update
+        puts "\n!- Author was deleted\n"
+      else
+        puts "\n!- Error!, Something went wrong\n\t"
+      end
+
+    elsif commands[1] == "Show Books"
+      puts "Show Books"
+    else
+      puts "\n!- Undefined command for this entity\n"
+    end
+
+  when "Exit"
+    isExit = true
+    puts "Bye"
+  else
+    puts "\n!- Wrong COMMAND\n"
   end
 end
--- app/services/author_service.rb
+++ app/services/author_service.rb
@@ -3,4 +3,67 @@ require 'json'
 
 module AuthorService
 
+  @json_name = "../json/data.json"
+
+  @json_new = @json_name + ".new"
+  @json_old = @json_name + ".old"
+
+  #add to json
+  def AuthorService.add_author?(name, biography)
+    begin
+      file = File.open("../json/data.json", "r")
+      data_hash = JSON.parse(file.read)
+      file.close
+
+      data_hash["Authors"] << {name: name, biography: biography}
+
+      File.write(@json_new,JSON.pretty_generate(data_hash))
+
+      File.rename(@json_name, @json_old)
+      File.rename(@json_new, @json_name)
+      File.delete(@json_old)
+    rescue
+       return false
+    end
+
+    return true
+  end
+
+  #delete from json and return the result
+  def AuthorService.delete_author?(author)
+    begin
+      file = File.open("../json/data.json", "r")
+      data_hash = JSON.parse(file.read)
+      file.close
+
+      data_hash["Authors"].delete_if { |h| h["name"] == author.name && h["biography"] == author.biography }
+
+      File.write(@json_new,JSON.pretty_generate(data_hash))
+
+      File.rename(@json_name, @json_old)
+      File.rename(@json_new, @json_name)
+      File.delete(@json_old)
+    rescue
+      return false
+    end
+
+    return true
+  end
+
+  #retrun all users from json
+  def AuthorService.get_all_authors
+    authors = []
+    File.open("../json/data.json") do |file|
+      date_hash = JSON.parse(file.read)
+
+      author_hash = date_hash["Authors"]
+
+      author_hash.each do |elem|
+        authors.push(Author.new(elem["name"], elem["biography"]))
+      end
+
+      return authors
+    end
+  end
+
 end
